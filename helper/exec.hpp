3f7c92ba7ed0c6eb0103666935ee5efb2dc3fcaf
Ruakij <ruakij@ruekov.eu>
2021-11-21 14:29:11 +0100

    First implementation

---
+++ helper/exec.hpp
@@ -0,0 +1,31 @@
+#ifndef B89BC3C5_AD59_4765_AA06_8110111D316F
+#define B89BC3C5_AD59_4765_AA06_8110111D316F
+
+#include <cstdio>
+#include <stdexcept>
+
+/// @brief Executes given command and optionally sends buffer to handler
+/// @param cmd is the command
+/// @param handler is the handler(char*)-function
+/// @return Return-code form command
+int exec(const char* cmd, const int *(handler)(char*) = nullptr){
+    const int buf_size = 256;
+    char buf[buf_size];
+
+    // Open execution-pipe
+    FILE *pipe = popen(cmd, "r");
+    
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buf, buf_size, pipe) != nullptr) {
+
+        // When a handler is specified, call it
+        if(handler != nullptr) (*handler)(buf);
+    }
+
+    // Close pipe and read exit-code
+    return WEXITSTATUS(pclose(pipe));
+}
+
+#endif /* B89BC3C5_AD59_4765_AA06_8110111D316F */
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string>
+#include "./helper/exec.hpp"
+
+
+const std::string tcpdump_baseCmd = "tcpdump -vvv -e -n -X -s0 -i ";
+
+int main(int argc, char *args[]){
+
+    std::string tcpdump_cmd;
+    if(argc == 2){
+        tcpdump_cmd = tcpdump_baseCmd + args[1];
+    } else {
+        fprintf(stderr, "Missing interface\n");
+        exit(1);
+    }
+
+    int exitCode = exec(tcpdump_cmd.c_str());
+    
+    if(exitCode){
+        fprintf(stderr, "\ntcpdump exited with non-zero ExitCode: %d\n Something went wrong! Check tcpdump-output for more information.\n", exitCode);
+        exit(1);
+    }
+
+    return 0;
+}
