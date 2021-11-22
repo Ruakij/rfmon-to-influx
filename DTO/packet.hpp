b8b2bb7202425bef9f75d3a9ab66ae2fe5f68046
Ruakij <ruakij@ruekov.eu>
2021-11-22 09:07:49 +0100

    Added basic packet-structure

---
+++ DTO/packet.hpp
@@ -0,0 +1,19 @@
+#ifndef C42FA9F6_8CF3_453F_8FA0_918E543DCD59
+#define C42FA9F6_8CF3_453F_8FA0_918E543DCD59
+
+#include <time.h>
+
+struct Packet {
+    time_t timestamp;
+
+    char srcMac[17];
+    char dstMac[17];
+
+    unsigned int size;
+    
+    char signal;
+    unsigned int frequency;
+    unsigned char linkSpeed;
+};
+
+#endif /* C42FA9F6_8CF3_453F_8FA0_918E543DCD59 */
