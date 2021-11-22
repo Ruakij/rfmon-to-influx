5ac832623d4c14d4f13930939c63bfa6678df8f4
Ruakij <ruakij@ruekov.eu>
2021-11-22 10:31:41 +0100

    Created empty TextPacketHandler

---
+++ handler/textPacketHandler.hpp
@@ -0,0 +1,11 @@
+#ifndef EE781A91_6D07_47AC_B3C4_F99E29F3731F
+#define EE781A91_6D07_47AC_B3C4_F99E29F3731F
+
+#include <string>
+#include <vector>
+
+void textPacketHandler(std::vector<std::string> textPacket){
+    /// Here we have to parse the packet
+}
+
+#endif /* EE781A91_6D07_47AC_B3C4_F99E29F3731F */
