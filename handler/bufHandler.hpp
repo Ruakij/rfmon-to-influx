16028daf1028d883de606b235f5c2f194c81e474
Ruakij <ruakij@ruekov.eu>
2021-11-22 15:23:53 +0100

    Auto-Remove newline-char

--- handler/bufHandler.hpp
+++ handler/bufHandler.hpp
@@ -9,16 +9,20 @@ using namespace std::string_literals;
 
 std::vector<std::string> buffer;
 void bufHandler(const char *buf){
+    std::string line = buf;
+    // Remove last char which is \n
+    line = line.substr(0, line.size()-1);
+
     // When first char of buf has text (no tab), we got a new packet
     if(buf[0] != '\t'){
         // Submit the just-read text-packet in a new thread
         if(buffer.size() != 0) {
             (void)std::async(std::launch::async, textPacketHandler, buffer);
         }
-        buffer = {std::string(buf)};
+        buffer = {line};
     }
     else
-        buffer.push_back(std::string(buf));     // Append part-packet
+        buffer.push_back(line);     // Append part-packet
 }
 
 #endif /* C251BA62_6D80_4033_86B6_61F184E6F250 */
