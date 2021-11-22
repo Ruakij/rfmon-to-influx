e7dcaf99a015c16446596fd3611e210fcc715110
Ruakij <ruakij@ruekov.eu>
2021-11-22 10:32:29 +0100

    Created empty PacketHandler

---
+++ handler/packetHandler.hpp
@@ -0,0 +1,10 @@
+#ifndef EA8E466A_DAAA_4747_9CEE_65B77A4EF694
+#define EA8E466A_DAAA_4747_9CEE_65B77A4EF694
+
+#include "../DTO/packet.hpp"
+
+void packetHandler(Packet packet){
+    
+}
+
+#endif /* EA8E466A_DAAA_4747_9CEE_65B77A4EF694 */
