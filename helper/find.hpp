c5bc468ab9f0f16ad1739d153c45d921f70fe6be
Ruakij <ruakij@ruekov.eu>
2021-11-22 11:31:13 +0100

    Added documentation to find

--- helper/find.hpp
+++ helper/find.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+/// @brief Internal function
 void prepare(const int &size, int &start, const int &offset, int &end){
     // Set missing fields
     if(!end) end = size;
@@ -15,6 +16,13 @@ void prepare(const int &size, int &start, const int &offset, int &end){
         end -= offset;
 }
 
+/// @brief Find str-index based on contains-content
+/// @param data is the vector-string-data to search
+/// @param strContains string to find
+/// @param start where to start searching
+/// @param offset search offset to position (results in index being shifted by -offset)
+/// @param end where to end searching
+/// @return index of found index (with offset if any)
 int findContains(const std::vector<std::string> &data, const std::string &strContains, int start = 0, int offset = 0, int end = 0){
     prepare(data.size(), start, offset, end);
 
@@ -25,6 +33,13 @@ int findContains(const std::vector<std::string> &data, const std::string &strCon
     return -1;
 }
 
+/// @brief Find str-index based on exact-content
+/// @param data is the vector-string-data to search
+/// @param strIs string to find (exact)
+/// @param start where to start searching
+/// @param offset search offset to position (results in index being shifted by -offset)
+/// @param end where to end searching
+/// @return index of found index (with offset if any)
 int findIs(const std::vector<std::string> &data, const std::string &strIs, int start = 0, int offset = 0, int end = 0){
     prepare(data.size(), start, offset, end);
 
