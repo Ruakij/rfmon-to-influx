67104420671c1619bad463c2ff71550b3ad3160c
Ruakij <ruakij@ruekov.eu>
2021-11-22 16:14:37 +0100

    Fixed missing imports

--- helper/split.hpp
+++ helper/split.hpp
@@ -1,6 +1,10 @@
 #ifndef F7CFE6A7_34BF_4E04_94CF_DB8374980631
 #define F7CFE6A7_34BF_4E04_94CF_DB8374980631
 
+#include <vector>
+#include <string>
+#include <sstream>
+
 std::vector<std::string> split(const std::string& s, char delimiter)
 {
    std::vector<std::string> tokens;
