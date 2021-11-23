739f27c0b811491d31055c6ed1974b6d38319bef
Ruakij <ruakij@ruekov.eu>
2021-11-23 12:51:13 +0100

    Added InfluxDB-client library Version 3.9.0 https://github.com/tobiasschuerg/InfluxDB-Client-for-Arduino

--- handler/packetHandler.hpp
+++ handler/packetHandler.hpp
@@ -4,7 +4,8 @@
 #include "../DTO/packet.hpp"
 
 void packetHandler(const Packet &packet){
-    
+    // Here we have to build influx-dataseries from the packet
+    //  Its probably a good idea to 
 }
 
 #endif /* EA8E466A_DAAA_4747_9CEE_65B77A4EF694 */
