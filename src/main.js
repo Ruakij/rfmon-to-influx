cc8b106157f4f7df6cdb7108ed7ad912d595473b
Ruakij <ruakij@ruekov.eu>
2021-11-24 13:10:41 +0100

    Added logger-setup-module

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-const log4js = require("log4js");
+const logger = require("./helper/logger.js")("main");
+
 const { requireEnvVars } = require("./helper/env.js");
 const { exit } = require("process");
-
-/// Setup logger
-const logger = log4js.getLogger("main");
-logger.level = process.env.LOGLEVEL ?? "INFO";
 
 /// Setup ENVs
 const env = process.env;
 // Defaults
 {
+  env.LOGLEVEL            ??= "INFO";
   env.WIFI_INTERFACE      ??= "wlan0";
   env.WIFI_CHANNEL        ??= [1,6,11];
   env.WIFI_CHANNEL_TIME   ??= 1;
