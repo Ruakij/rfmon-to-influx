1b6721d24225d3a50ff625345fe5ed2ea2feaadb
Ruakij <ruakij@ruekov.eu>
2021-11-24 01:07:29 +0100

    Implemented env defaults and checking

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 const log4js = require("log4js");
+const { requireEnvVars } = require("./helper/env.js");
 
 /// Setup logger
 const logger = log4js.getLogger("main");
 logger.level = process.env.logLevel ?? "INFO";
 
+/// Setup ENVs
+const env = process.env;
+// Defaults
+{
+  env.WIFI_INTERFACE      ??= "wlan0";
+  env.WIFI_CHANNEL        ??= [1,6,11];
+  env.WIFI_CHANNEL_TIME   ??= 1;
+}
+// Required vars
+let errorMsg = requireEnvVars([
+  "INFLUX_URL", "INFLUX_TOKEN",
+  "INFLUX_ORG", "INFLUX_BUCKET"
+]);
+if(errorMsg){
+  logger.fatal(errorMsg);
+  exit(1);
+}
 
