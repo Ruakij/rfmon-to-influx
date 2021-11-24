7dd131d6b54c408d08bfe757f71dda6342df546d
Ruakij <ruakij@ruekov.eu>
2021-11-24 01:11:42 +0100

    Implemented dto-obj Packet

---
+++ src/dto/Packet.js
@@ -0,0 +1,46 @@
+const PacketType = {
+    Beacon: 'Beacon',
+    ProbeRequest: 'ProbeRequest',
+    ProbeResponse: 'ProbeResponse',
+    Data: 'Data',
+    RequestToSend: 'RequestToSend',
+    ClearToSend: 'ClearToSend',
+    Acknowledgment: 'Acknowledgment',
+    BlockAcknowledgment: 'BlockAcknowledgment',
+    NoData: 'NoData',
+    Unknown: 'Unknown'
+}
+
+class Packet{
+    timestampMicros;
+
+    srcMac;
+    dstMac;
+    bssid;
+
+    signal;
+    frequency;
+    dataRate;
+
+    packetType;
+}
+
+// Extensions of Packet
+class PacketWithSSID extends Packet{
+    ssid;
+}
+
+class BeaconPacket extends PacketWithSSID{}
+class ProbeRequestPacket extends PacketWithSSID{}
+class ProbeResponsePacket extends PacketWithSSID{}
+
+
+// Specify exports
+module.exports = {
+    PacketType,
+    Packet,
+    PacketWithSSID,
+    BeaconPacket,
+    ProbeRequestPacket,
+    ProbeResponsePacket
+};
