dcd0ce8111613025aa3ae9152a62e7c6cff5d1c6
Ruakij <ruakij@ruekov.eu>
2021-11-25 18:32:43 +0100

    Added lib string-argv to extract bin and args from cmd

--- src/helper/exec.js
+++ src/helper/exec.js
@@ -1,9 +1,11 @@
 const logger = require("./logger.js")("exec");
 
 const { spawn } = require("child_process");
+const { parseArgsStringToArgv } = require('string-argv');
 
 
 function exec(cmd, options){
+    const [bin, ...args] = parseArgsStringToArgv(cmd);
 
     logger.addContext("binary", "bin");
     logger.debug(`Spawn process '${cmd}'`);
