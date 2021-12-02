b98dff947d6f899a8980606349616782e628bf11
Ruakij <ruakij@ruekov.eu>
2021-12-02 14:54:32 +0100

    Refactored packet to use new code-style (for got last time :/)

--- src/dto/Packet.js
+++ src/dto/Packet.js
@@ -1,20 +1,20 @@
 const PacketType = {
-    Beacon: 'Beacon',
-    ProbeRequest: 'ProbeRequest',
-    ProbeResponse: 'ProbeResponse',
-    Data: 'Data',
-    RequestToSend: 'RequestToSend',
-    ClearToSend: 'ClearToSend',
-    Acknowledgment: 'Acknowledgment',
-    BlockAcknowledgment: 'BlockAcknowledgment',
-    NoData: 'NoData',
-    Authentication: 'Authentication',
-    AssociationRequest: 'AssociationRequest',
-    AssociationResponse: 'AssociationResponse',
-    Disassociation: 'Disassociation',
-    Handshake: 'Handshake',
-    Unknown: 'Unknown'
-}
+    Beacon: "Beacon",
+    ProbeRequest: "ProbeRequest",
+    ProbeResponse: "ProbeResponse",
+    Data: "Data",
+    RequestToSend: "RequestToSend",
+    ClearToSend: "ClearToSend",
+    Acknowledgment: "Acknowledgment",
+    BlockAcknowledgment: "BlockAcknowledgment",
+    NoData: "NoData",
+    Authentication: "Authentication",
+    AssociationRequest: "AssociationRequest",
+    AssociationResponse: "AssociationResponse",
+    Disassociation: "Disassociation",
+    Handshake: "Handshake",
+    Unknown: "Unknown"
+};
 
 const FlagType = {
     MoreFragments: "MoreFragments",
@@ -23,7 +23,7 @@ const FlagType = {
     MoreData: "MoreData",
     Protected: "Protected",
     Order: "Order"
-}
+};
 
 class Packet{
     timestampMicros;
@@ -58,9 +58,9 @@ class ProbeRequestPacket extends PacketWithSSID{}
 class ProbeResponsePacket extends PacketWithSSID{}
 
 const AuthenticationType = {
-    OpenSystem_1: 'OpenSystem_1',
-    OpenSystem_2: 'OpenSystem_2',
-    Unknown: 'Unknown',
+    OpenSystem_1: "OpenSystem_1",
+    OpenSystem_2: "OpenSystem_2",
+    Unknown: "Unknown",
 }
 class AuthenticationPacket extends Packet{
     authenticationType;
@@ -77,10 +77,10 @@ class DisassociationPacket extends Packet{
 
 
 const HandshakeStage = {
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4'
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4"
 }
 class HandshakePacket extends Packet{
     handshakeStage;
